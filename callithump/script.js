583a7fdffec5cedfadae6973bd2e859094521e8d
charlotte <parent.gabriel.grp@gmail.com>
2026-06-15 20:34:48 -0400

    wip

--- callithump/script.js
+++ callithump/script.js
@@ -2,6 +2,9 @@ window.onload = init;
 
 function init() {
     lyrics.songs.forEach((song) => {
+    var details = document.createElement("details");
+    document.getElementById("container").appendChild(details);
+
     var title = document.createElement("p");
     title.innerText = song.name;
     var summaryDiv = document.createElement("div");
@@ -10,22 +13,35 @@ function init() {
     var summary = document.createElement("summary");
     summary.appendChild(summaryDiv)
 
+    details.appendChild(summary);
+
     var note = document.createElement("h2");
     note.innerHTML = song.note;
     var lyricsDiv = document.createElement("div");
     lyricsDiv.className = "lyrics";
     lyricsDiv.appendChild(note);
 
-    song.lyrics.forEach((lyric) => {
-        var lyricP = document.createElement("p");
-        for(let i = 0; i < lyric.lines.length; i++) {
-            lyricP.innerHTML += (i > 0 ? "<br>" : "") + lyric.lines[i] + ((lyric.lines[i] == "" && i == lyric.lines.length - 1) ? "<br>" : "");
-        }
-        lyricsDiv.appendChild(lyricP);
-    });
-
-    var details = document.createElement("details");
-    details.appendChild(summary);
     details.appendChild(lyricsDiv);
-    document.getElementById("container").appendChild(details);
+
+    song.lyrics.forEach((lyric) => {
+        var lyricDiv = document.createElement("div");
+        var commentsDiv = document.createElement("div");
+        if(lyric.comments[0] != "") {
+            lyricDiv.className = "hascomments";
+            commentsDiv.className = "comments";
+            lyricDiv.appendChild(commentsDiv);
+            for (let i = 0; i < lyric.comments.length; i++) {
+                var commentP = document.createElement("p");
+                commentP.innerHTML = lyric.comments[i];
+                commentsDiv.appendChild(commentP);
+            }
+        }
+        for(let i = 0; i < lyric.lines.length; i++) {
+            var lyricP = document.createElement("p");
+            // lyricP.innerHTML += (i > 0 ? "<br>" : "") + lyric.lines[i] + ((lyric.lines[i] == "" && i == lyric.lines.length - 1) ? "<br>" : "");
+            lyricP.innerHTML += lyric.lines[i] + (lyric.lines[i] == "" ? "<br>" : "");
+            lyricDiv.appendChild(lyricP);
+        }
+        lyricsDiv.appendChild(lyricDiv);
+    });
 });}
